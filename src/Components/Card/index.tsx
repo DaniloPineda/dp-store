b71d0965c2488d9b2147940d8f8d0e56c43f5c7e
Danilo Pineda <danilopineda93@gmail.com>
2024-01-20 17:06:41 -0600

    add styles

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -5,11 +5,11 @@ const Card = ({prod}: {prod: Product}) => {
         <div className="bg-white w-56 h-60">
             <figure className="relative mb-2 w-full h-4/5">
                 <span className="absolute bottom-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{prod.category.name}</span>
-                <img className="w-full h-full object-cover rounded-lg " 
+                <img className="w-full h-full object-cover rounded-t-lg " 
                         src={prod.images?.[0]} alt={prod.title} />
                 <div className="absolute cursor-pointer top-0 right-0 flex justify-center align-middle bg-secondary w-6 h-6 rounded-full font-bold m-2">+</div>
             </figure>
-            <p className="flex justify-between border-solid border-b-2 border-x-2 border-primary rounded shadow-md">
+            <p className="flex justify-between border-solid border-b-2 border-x-2 border-primary rounded-b-lg shadow-2xl py-1">
                 <span className="text-sm font-light pl-2">{prod.title}</span>
                 <span className="test-lg font-medium pr-2">{prod.price}</span>
             </p>
