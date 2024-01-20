f61ec98cba86dd2daf6a1ca9a257048d9789a518
Danilo Pineda <danilopineda93@gmail.com>
2024-01-20 15:40:40 -0600

    add temp collections

---
+++ src/Collections/products.js
@@ -0,0 +1,198 @@
+[
+    {
+        id: 1,
+        title: "Cappuccino",
+        price: 5.50,
+        description: "A classic Italian espresso drink with steamed milk and a frothy top.",
+        category: {
+          id: 1,
+          name: "Drinks",
+          image: "https://placeimg.com/640/480/any?r=0.591926261873231"
+        },
+        images: [
+          "https://images.pexels.com/photos/414555/pexels-photo-414555.jpeg"
+        ]
+    },
+    {
+        id: 2,
+        title: "Colombian Cold Brew",
+        price: 4.25,
+        description: "Smooth and refreshing cold brew coffee, made with Colombian beans and steeped for 18 hours.",
+        category: {
+            id: 1,
+            name: "Drinks",
+            image: "https://placeimg.com/640/480/any?r=0.591926261873231"
+        },
+        images: [
+            "https://images.pexels.com/photos/4354635/pexels-photo-4354635.jpeg"
+        ]
+    },
+    {
+        id: 3,
+        title: "Latte",
+        price: 4.25,
+        description: "Smooth and refreshing cold brew coffee, made with Colombian beans and steeped for 18 hours.",
+        category: {
+            id: 1,
+            name: "Drinks",
+            image: "https://placeimg.com/640/480/any?r=0.591926261873231"
+        },
+        images: [
+            "https://images.pexels.com/photos/4354635/pexels-photo-4354635.jpeg"
+        ]
+    },
+    {
+        id: 4,
+        title: "Aguapanela",
+        price: 4.25,
+        description: "Smooth and refreshing cold brew coffee, made with Colombian beans and steeped for 18 hours.",
+        category: {
+            id: 1,
+            name: "Drinks",
+            image: "https://placeimg.com/640/480/any?r=0.591926261873231"
+        },
+        images: [
+            "https://images.pexels.com/photos/4354635/pexels-photo-4354635.jpeg"
+        ]
+    },
+    {
+        id: 5,
+        title: "Mango Smoothie",
+        price: 3.25,
+        description: "Soft and cheesy Colombian rolls, perfect for a light breakfast or snack.",
+        category: {
+            id: 2,
+            name: "Foods",
+            image: "https://placeimg.com/640/480/any?r=0.4695465546554655"
+        },
+        images: [
+            "https://images.pexels.com/photos/3254625/pexels-photo-3254625.jpeg"
+        ]
+    },
+    {
+        id: 6,
+        title: "Pan de Bono",
+        price: 4.95,
+        description: "A classic Colombian dessert soaked in three milks, with a light and airy texture.",
+        category: {
+            id: 3,
+            name: "Desserts",
+            image: "https://placeimg.com/640/480/any?r=0.9546546546546547"
+        },
+        images: [
+            "https://images.pexels.com/photos/5464546/pexels-photo-5464546.jpeg"
+        ]
+    },
+    {
+        id: 5,
+        title: "Arepa con Queso",
+        price: 5.95,
+        description: "A grilled cornmeal flatbread filled with gooey cheese, a Colombian staple.",
+        category: {
+            id: 2,
+            name: "Foods",
+            image: "https://placeimg.com/640/480/any?r=0.4695465546554655"
+        },
+        images: [
+            "https://images.pexels.com/photos/546454654/pexels-photo-546454654.jpeg"
+        ]
+    },
+    {
+        id: 7,
+        title: "Arepa con Queso",
+        price: 3.75,
+        description: "A creamy, custard-like dessert with a rich caramel sauce.",
+        category: {
+            id: 3,
+            name: "Desserts",
+            image: "https://placeimg.com/640/480/any?r=0.9546546546546547"
+        },
+        images: [
+            "https://images.pexels.com/photos/3226884/pexels-photo-3226884.jpeg"
+        ]
+    },
+    {
+        id: 8,
+        title: "Empanada",
+        price: 3.75,
+        description: "A creamy, custard-like dessert with a rich caramel sauce.",
+        category: {
+            id: 3,
+            name: "Desserts",
+            image: "https://placeimg.com/640/480/any?r=0.9546546546546547"
+        },
+        images: [
+            "https://images.pexels.com/photos/3226884/pexels-photo-3226884.jpeg"
+        ]
+    },
+    {
+        id: 8,
+        title: "Patacones con Hogao",
+        price: 3.75,
+        description: "A creamy, custard-like dessert with a rich caramel sauce.",
+        category: {
+            id: 3,
+            name: "Desserts",
+            image: "https://placeimg.com/640/480/any?r=0.9546546546546547"
+        },
+        images: [
+            "https://images.pexels.com/photos/3226884/pexels-photo-3226884.jpeg"
+        ]
+    },
+    {
+        id: 9,
+        title: "Tres Leches Cake",
+        price: 3.75,
+        description: "A creamy, custard-like dessert with a rich caramel sauce.",
+        category: {
+            id: 3,
+            name: "Desserts",
+            image: "https://placeimg.com/640/480/any?r=0.9546546546546547"
+        },
+        images: [
+            "https://images.pexels.com/photos/3226884/pexels-photo-3226884.jpeg"
+        ]
+    },
+    {
+        id: 10,
+        title: "Flan de Caramelo",
+        price: 3.75,
+        description: "A creamy, custard-like dessert with a rich caramel sauce.",
+        category: {
+            id: 3,
+            name: "Desserts",
+            image: "https://placeimg.com/640/480/any?r=0.9546546546546547"
+        },
+        images: [
+            "https://images.pexels.com/photos/3226884/pexels-photo-3226884.jpeg"
+        ]
+    },
+    {
+        id: 11,
+        title: "Arroz con Leche",
+        price: 3.75,
+        description: "A creamy, custard-like dessert with a rich caramel sauce.",
+        category: {
+            id: 3,
+            name: "Desserts",
+            image: "https://placeimg.com/640/480/any?r=0.9546546546546547"
+        },
+        images: [
+            "https://images.pexels.com/photos/3226884/pexels-photo-3226884.jpeg"
+        ]
+    },
+    {
+        id: 12,
+        title: "Buñuelos",
+        price: 3.75,
+        description: "A creamy, custard-like dessert with a rich caramel sauce.",
+        category: {
+            id: 3,
+            name: "Desserts",
+            image: "https://placeimg.com/640/480/any?r=0.9546546546546547"
+        },
+        images: [
+            "https://images.pexels.com/photos/3226884/pexels-photo-3226884.jpeg"
+        ]
+    }
+]
